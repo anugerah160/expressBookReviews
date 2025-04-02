198f538addb6e60f4f8b03c659116bdf7e51415c
anugerah160 <artuphareguna10@gmail.com>
2025-04-01 21:43:41 -0400

    done

--- final_project/index.js
+++ final_project/index.js
@@ -5,13 +5,27 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
+const SECRET_KEY = "fingerprint_customer";
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",session({secret:SECRET_KEY,resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    //Write the authenication mechanism here
+    const token = req.session.token || req.headers["authorization"];
+
+    if(!token){
+        return res.status(403).json({message: "Access denied, No token."})
+    }
+
+    jwt.verify(token.split(" ")[1], SECRET_KEY, (err, decoded) => {
+        if(err){
+            return res.status(401).json({message: "Invalid token"});
+        }
+        req.user = decoded;
+        next();
+    })
 });
  
 const PORT =5000;
@@ -19,4 +33,4 @@ const PORT =5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,73 @@ const regd_users = express.Router();
 
 let users = [];
 
+const SECRET_KEY = "fingerprint_customer";
+
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+    return username && !users.some(user => user.username === username);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    return users.some(user => user.username === username && user.password === password);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {username, password} = req.body;
+
+  if(!username && !password){
+    return res.status(400).json({message: "Username and Password are required"})
+  }
+  if(!authenticatedUser(username, password)){
+    return res.status(401).json({message: "Invalid username or password"})
+  }
+
+  const token = jwt.sign({username}, SECRET_KEY, {expiresIn: "2h"})
+  req.session.token = token;
+
+  return res.status(200).json({message: "Login successful", token});
 });
 
+const verifyToken = (req, res, next) => {
+    const token = req.session.token || req.headers["authorization"];
+
+    if(!token){
+        return res.status(403).json({message: "Access denied, No token."})
+    }
+
+    jwt.verify(token.split(" ")[1], SECRET_KEY, (err, decoded) => {
+        if(err){
+            return res.status(401).json({message: "Invalid token"});
+        }
+        req.user = decoded;
+        next()
+    })
+}
+
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
+regd_users.put("/auth/review/:isbn", verifyToken,(req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {isbn} = req.params;
+  const {review} = req.body;
+  const username = req.user.username;
+
+  if(!books[isbn]){
+    return res.status(404).json({message: "User not found"});
+  }
+  if(!review){
+    return res.status(400).json({message: "Review content is required"})
+  }
+
+  if(!books[isbn].review){
+    books[isbn].reviews = {};
+  }
+
+  books[isbn].reviews[username] = review;
+
+  return res.status(200).json({message: "Review added/updated successfully ", reviews: books[isbn].reviews});
 });
 
 module.exports.authenticated = regd_users;
